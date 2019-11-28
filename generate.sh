6e5b8dda68f76ad035835be306f59d1dfcc18f46
SorboRail <sorborail@yahoo.com>
2019-11-29 01:19:12 +0300

    Added game engine proto and pb files

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 protoc -I highscorepb/v1 highscorepb/v1/highscore.proto --go_out=plugins=grpc:highscorepb/v1
+
+protoc -I game-enginepb/v1 game-enginepb/v1/game-engine.proto --go_out=plugins=grpc:game-enginepb/v1
